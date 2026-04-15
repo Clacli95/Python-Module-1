c58b10d9e6c6e0e96135c3a9e83ecadf2fc4081f
Clacli95 <claudia.salvatore@outlook.it>
2026-04-15 14:52:41 +0200

    liste

---
+++ Modulo 1_From Zero to Hero/for_esercizio1.py
@@ -0,0 +1,5 @@
+a="Carolina"
+b="Beatrice"
+c="Gertrude"
+for i, val in enumerate ([a,b,c]):
+    print(i,val)
---
+++ Modulo 1_From Zero to Hero/liste.py
@@ -0,0 +1,9 @@
+elenco=[1,2,3,4,5]
+elenco.append(8)
+print(elenco)
+elenco.remove(2)
+print(elenco)
+elenco.insert(2, 7)
+print(elenco)
+
+
